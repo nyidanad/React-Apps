939340382aabea1866b77850834ef72569a9e8f6
Nyíri Dániel <91250016+nyidanad@users.noreply.github.com>
2024-04-15 14:47:44 +0200

    added: creating tags & storing notes
    creating new tag;
    creating useLocalStorage Hook;
    storing notes in LocalStorage

--- markdown-vite-react-app/src/App.tsx
+++ markdown-vite-react-app/src/App.tsx
@@ -1,16 +1,70 @@
+import { useMemo } from "react"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import AddMarkdown from "./pages/AddMarkdown"
 import ShowMarkdown from "./pages/ShowMarkdown"
 import EditMarkdown from "./pages/EditMarkdown"
+import useLocalStorage from "./hooks/useLocalStorage"
 import { Container } from "react-bootstrap"
+import { v4 as uuidV4 } from "uuid"
+
+
+
+export type Note = {
+  id: string,
+} & NoteData
+
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  // CACHE NOTES
+  const noteWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  // CREATE NOTE
+  const onCreateNote = ({ tags, ...data }: NoteData) => {
+    setNotes(prevNotes => {
+      return [...prevNotes, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }]
+    })
+  } 
+
+  // ADD TAG
+  const onAddTag = (tag: Tag) => {
+    setTags(prev => [...prev, tag])
+  }
+
+
   return (
     <Container className="mb-4">  
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/add" element={<AddMarkdown />}/>
+        <Route path="/add" element={<AddMarkdown onSubmit={onCreateNote} onAddTag={onAddTag} availableTags={tags} />}/>
         <Route path="/:id">
           <Route index element={<ShowMarkdown />} />
           <Route path="edit" element={<EditMarkdown />} />
--- markdown-vite-react-app/src/pages/AddMarkdown.tsx
+++ markdown-vite-react-app/src/pages/AddMarkdown.tsx
@@ -1,21 +1,42 @@
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import CreatableSelect from 'react-select/creatable';
-import tags from "../data/tags.json";
 import { useNavigate } from "react-router-dom";
+import { FormEvent, useRef, useState } from "react";
+import { NoteData, Tag } from "../App";
+import { v4 as uuidV4 } from "uuid"
 
-function AddMarkdown() {
+type AddMarkdownProps = {
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+function AddMarkdown({ onSubmit, onAddTag, availableTags }: AddMarkdownProps) {
   const navigate = useNavigate();
+  const titleRef = useRef<HTMLInputElement>(null);
+  const bodyRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    onSubmit({
+      title: titleRef.current!.value,    // nem lehet null, mivel 'required'
+      markdown: bodyRef.current!.value,  // nem lehet null, mivel 'required'
+      tags: selectedTags
+    })
+  }
 
   return (
     <div>
       <h3 className="my-4">Add File</h3>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Stack gap={4}>
           <Row>
             <Col>
               <Form.Group controlId="title">
                 <Form.Label>Title</Form.Label>
-                <Form.Control required />
+                <Form.Control ref={titleRef} required />
               </Form.Group>
             </Col>
             <Col>
@@ -26,16 +47,27 @@ function AddMarkdown() {
                   isClearable
                   closeMenuOnSelect={false}
                   name="tags"
-                  options={tags}
+                  options={availableTags.map(tag => { return { label: tag.label, value: tag.id } })}
                   className="basic-multi-select"
                   classNamePrefix="select"
+                  value={selectedTags.map(tag => {return { label: tag.label, value: tag.id }})}
+                  onChange={tags => {
+                    setSelectedTags(tags.map(tag => {
+                      return { label: tag.label, id: tag.value }
+                    }))
+                  }}
+                  onCreateOption = {label => {
+                    const newTag = { id: uuidV4(), label }
+                    onAddTag(newTag)
+                    setSelectedTags(prev => [...prev, newTag])
+                  }}
                 />
               </Form.Group>
             </Col>
           </Row>
           <Form.Group controlId="markdown">
             <Form.Label>Body</Form.Label>
-            <Form.Control required as="textarea" rows={15} />
+            <Form.Control ref={bodyRef} required as="textarea" style={{ minHeight: '25rem' }}/>
           </Form.Group>
         </Stack>
         <Stack direction="horizontal" gap={2} className="justify-content-end mt-3">
